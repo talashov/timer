ac697762294dd167b965fb9a6a309aebd8362353
Nikita <ntalashov@gmail.com>
2022-12-27 01:15:27 +0600

    add file

---
+++ js/main.js
@@ -0,0 +1,23 @@
+const countDown = document.querySelector(".countdown");
+const startBtn = document.querySelector(".start__btn");
+const stopBtn = document.querySelector(".stop__btn");
+
+let number = 60;
+let timerId = null;
+
+startBtn.onclick = function () {
+  clearInterval(timerId);
+  timerId = setInterval(() => {
+    number--;
+    countDown.innerHTML = number;
+    if (number === 0) {
+      clearInterval(timerId);
+    }
+  }, 1000);
+};
+
+stopBtn.onclick = function () {
+  clearInterval(timerId);
+  number = 0;
+  countDown.innerHTML = number;
+}
