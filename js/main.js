2d26e6a89b28a1404d8085426eecaddec35270ce
Nikita <ntalashov@gmail.com>
2022-12-27 01:39:45 +0600

    upd js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,20 @@
 const countDown = document.querySelector(".countdown");
+const approach = document.querySelector(".approach");
 const startBtn = document.querySelector(".start__btn");
 const stopBtn = document.querySelector(".stop__btn");
 
 let number = 60;
 let timerId = null;
+let approachValue = 0;
 
 startBtn.onclick = function () {
+  
+  approachValue++;
   clearInterval(timerId);
+
   timerId = setInterval(() => {
     number--;
+    approach.innerHTML = (`Подход: ${approachValue}`);
     countDown.innerHTML = number;
     if (number === 0) {
       clearInterval(timerId);
@@ -19,5 +25,7 @@ startBtn.onclick = function () {
 stopBtn.onclick = function () {
   clearInterval(timerId);
   number = 60;
+  approachValue = 0;
   countDown.innerHTML = number;
+  approach.innerHTML = approachValue;
 }
